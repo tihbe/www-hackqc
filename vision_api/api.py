9958e21f797e404236fe9c995cf9549657191bbe
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-05-05 10:54:36 -0400

    Tornado file upload

--- vision_api/api.py
+++ vision_api/api.py
@@ -1,3 +1,6 @@
+# Thanks to https://technobeans.com/2012/09/17/tornado-file-uploads/ for file upload strategy
+# And https://github.com/matterport/Mask_RCNN for object detection
+
 
 import sys
 import os
@@ -6,6 +9,9 @@ import datetime
 import numpy as np
 from PIL import Image
 from bson import json_util
+import tornado
+import tornado.ioloop
+import tornado.web
 
 ROOT_DIR = sys.path[0]
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "./mask_rcnn_coco.h5")
@@ -19,6 +25,8 @@ import mrcnn.model as modellib
 from mrcnn import utils
 from mrcnn import visualize
 import coco
+
+PORT = 8888
 
 class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                 'bus', 'train', 'truck', 'boat', 'traffic light',
@@ -36,8 +44,6 @@ class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
                 'teddy bear', 'hair drier', 'toothbrush']
 
-
-
 class  InferenceConfig (coco.CocoConfig):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
@@ -47,49 +53,53 @@ config = InferenceConfig()
 model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 model.load_weights(COCO_MODEL_PATH, by_name=True) 
 
-
-image = np.array(Image.open("test_img.jpg"))
-#image = np.array(Image.open(BytesIO(img)))
-
-height, width, _ = image.shape
-timestamp = '{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now())
-results = model.detect([image])
-
-if len(results) != 1:
-    #error
-    pass
-
-result = results[0]
-
-# r = results[0]
-# visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
-
-result_obj = {
-    "timestamp": timestamp,
-    "image": {
-        "height": height,
-        "width": width
-    },
-    "objects": []
-}
-object_array = []
+class Userform(tornado.web.RequestHandler):
+    def get(self):
+        self.render("fileuploadform.html")
 
 
-for current_id, (class_id, bounding_box) in enumerate(zip(result['class_ids'], result['rois'])):
-    object_area = 0 
-    current_object = {
-        "category_id": int(class_id),
-        "bbox": bounding_box.tolist(),
-        "category": class_names[class_id],
-        "segment": [],
-        "id" : current_id,
-        "area": object_area
-    }
-    object_array.append(current_object)
+class Upload(tornado.web.RequestHandler):
+    def post(self):
+        fileinfo = self.request.files['filearg'][0]
+        image = np.array(Image.open(BytesIO(fileinfo['body'])))
+        height, width, _ = image.shape
+        timestamp = '{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now())
+        results = model.detect([image])
+        if len(results) != 1:
+            #error
+            pass
+        result = results[0]
+        result_obj = {
+            "timestamp": timestamp,
+            "image": {
+                "height": height,
+                "width": width
+            },
+            "objects": []
+        }
+        object_array = []
+
+        for current_id, (class_id, bounding_box) in enumerate(zip(result['class_ids'], result['rois'])):
+            object_area = 0 
+            current_object = {
+                "category_id": int(class_id),
+                "bbox": bounding_box.tolist(),
+                "category": class_names[class_id],
+                "segment": [],
+                "id" : current_id,
+                "area": object_area
+            }
+            object_array.append(current_object)
+
+        result_obj['objects'] = object_array
+        self.finish(json_util.dumps(result_obj))
 
 
-result_obj['objects'] = object_array
-to_return = json_util.dumps(result_obj)
+application = tornado.web.Application([
+        (r"/", Upload),
+        (r"/upload", Userform),
+        ], debug=False)
 
-
-print(to_return)
+if __name__ == "__main__":
+    application.listen(PORT)
+    tornado.ioloop.IOLoop.instance().start()
